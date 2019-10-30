2c434e930622bfc8b63c3eb15e1570b2346235f1
aox.lei <2387813033@qq.com>
2019-10-30 11:02:03 +0800

    增加example

---
+++ examples/example/example/spiders/ccidcom.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy_rabbitmq_scheduler.spiders import RabbitSpider
+from example.items import ArticleItem
+
+
+class CcidcomSpider(RabbitSpider):
+    name = 'ccidcom'
+    allowed_domains = ['ccidcom.com']
+    start_urls = ['http://www.ccidcom.com/']
+    # 队列名称
+    queue_name = 'ccidcom'
+    # item队列名称
+    items_key = 'item_ccidcom'
+
+    def parse(self, response):
+        navigation_list = response.css(
+            '#nav > div.nav-main.clearfix > ul > li > div > a::attr("href")')
+        for _link in navigation_list:
+            yield response.follow(_link,
+                                  dont_filter=True,
+                                  callback=self.parse_list)
+
+    def parse_list(self, response):
+        article_list = response.css('div.article-item')
+        for info in article_list:
+            item = ArticleItem()
+            item['title'] = info.css('div.title a>font::text').get()
+            item['url'] = info.css('div.title a::attr("href")').get()
+            yield item
