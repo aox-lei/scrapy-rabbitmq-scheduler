1638b49addc937e455e94426d8e9d17903edf2dd
admin <guolei>
2020-03-01 21:31:42 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, find_packages
 setup(
     # 以下为必需参数
     name='scrapy-rabbitmq-scheduler',  # 模块名
-    version='1.0.3',  # 当前版本
+    version='1.0.4',  # 当前版本
     description='Rabbitmq for Distributed scraping',  # 简短描述
+    author='aox lei',
     author_email='2387813033@qq.com',
     license='MIT',
     url='https://github.com/aox-lei/scrapy-rabbitmq-scheduler',
@@ -13,5 +14,5 @@ setup(
         'Scrapy>=0.14'
     ],
     packages=['scrapy_rabbitmq_scheduler'],
-    package_dir={'':'src'}
+    package_dir={'': 'src'}
 )
